4bbeffa8ab64f5e56146e2ca1f2dec23a142d278
Sapar Oralbek <190109012@stu.sdu.edu.kz>
2021-10-27 00:28:01 +0600

    update addItem screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LOFO',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData(//.withOpacity(0.1)
+        primarySwatch: Colors.lightBlue,
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: '/slider',
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         '/main': (context) => const HomeWidget(),
         '/login': (context) => const SigninWidget(),
         '/signup': (context) => const SignupWidget(),
-        '/item_information': (context) => const LoFoItemInfo(),
+        '/item_information': (context) => const LoFoItemInfo()
       },
     );
   }
---
+++ lib/widgets/add_item/add_item_page.dart
@@ -0,0 +1,214 @@
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:lofo_app/widgets/login/styled_widgets.dart';
+
+
+class AddItemPageWidget extends StatefulWidget {
+  const AddItemPageWidget({Key? key}) : super(key: key);
+
+  @override
+  _AddItemPageWidgetState createState() => _AddItemPageWidgetState();
+}
+
+class _AddItemPageWidgetState extends State<AddItemPageWidget> {
+
+  String dropdownValue = 'Lost';
+  File? _selectedFile;
+
+  Widget getImageWidget(){
+    if(null == _selectedFile){
+      return Image.asset(
+        "assets/image/add.png",
+        width: 300,
+        height: 105,
+        fit: BoxFit.cover,
+      );
+    }
+    else{
+      return Image.file(
+        _selectedFile!,
+        width: 300,
+        height: 105,
+        fit: BoxFit.cover,
+      );
+    }
+  }
+  List <String> spinnerItems = ['Lost', 'Found'] ;
+
+  void changed(String data){
+    setState(() {
+      dropdownValue = data;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset : false,
+        body: Container(color: Colors.blueAccent.withOpacity(0.1),
+          child:
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    getImageWidget()
+                    // Image.asset('assets/image/add.png')//assets/image/add.png
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(top: 10),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(onPressed:(){
+                    }, child:Text("from camera", style: TextStyle(color: Colors.white))),
+                    ElevatedButton(onPressed:(){
+                    }, child:Text("from device", style: TextStyle(color: Colors.white),)),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.only(top: 10),),
+                const Text("Description", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                Padding(padding: EdgeInsets.only(top: 10),),
+                const SizedBox(
+                  width: 325,
+                  height: 50,
+                  child: TextField(
+                    cursorColor: Color(0xffC19354),
+
+                    // controller: _weightController,
+                    decoration: InputDecoration(
+                        hoverColor: Color(0xffC19354),
+                        focusColor: Color(0xffC19354),
+                        // hintText: "weight (kilograms)",
+                        labelText: "Title",
+                        // labelStyle:
+                        border: OutlineInputBorder()),
+                    keyboardType: TextInputType.text,
+                    obscureText: false,
+                    // maxLines: 3,
+                    // maxLines: 3,
+                    // suffixIcon:
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 20),),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 35,right: 35),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black26, width: 1),
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      dropdownColor: Colors.white,
+                      icon: Icon(Icons.arrow_drop_down),
+                      iconSize: 24,
+                      isExpanded: true,
+                      underline: SizedBox(
+                      ),
+                      // elevation: 16,
+                      // style: TextStyle(color: Colors.lightBlueAccent, fontSize: 14),
+                      // underline: Container(
+                      //   height: 2,
+                      //   color: Colors.lightBlueAccent,
+                      // ),
+                      onChanged: (newValue){
+                        setState((){
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: spinnerItems.map((String value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 20),),
+
+                const SizedBox(
+                  width: 325,
+                  height: 50,
+                  child: TextField(
+                    cursorColor: Color(0xffC19354),
+
+                    // controller: _weightController,
+                    decoration: InputDecoration(
+                        hoverColor: Color(0xffC19354),
+                        focusColor: Color(0xffC19354),
+                        // hintText: "weight (kilograms)",
+                        labelText: "Time",
+                        // labelStyle:
+                        border: OutlineInputBorder()),
+                    keyboardType: TextInputType.datetime,
+                    obscureText: false,
+                    // maxLines: 3,
+                    // maxLines: 3,
+                    // suffixIcon:
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 20),),
+                const SizedBox(
+                  width: 325,
+                  height: 50,
+                  child: TextField(
+                    cursorColor: Color(0xffC19354),
+
+                    // controller: _weightController,
+                    decoration: InputDecoration(
+                        hoverColor: Color(0xffC19354),
+                        focusColor: Color(0xffC19354),
+                        // hintText: "weight (kilograms)",
+                        labelText: "Place",
+                        // labelStyle:
+                        border: OutlineInputBorder()),
+                    keyboardType: TextInputType.streetAddress,
+                    obscureText: false,
+                    // maxLines: 3,
+                    // maxLines: 3,
+                    // suffixIcon:
+                  ),
+                ),
+
+                Padding(padding: EdgeInsets.only(top: 20),),
+                const SizedBox(
+                  width: 325,
+                  height: 50,
+                  child: TextField(
+                    cursorColor: Color(0xffC19354),
+
+                    // controller: _weightController,
+                    decoration: InputDecoration(
+                        hoverColor: Color(0xffC19354),
+                        focusColor: Color(0xffC19354),
+                        // hintText: "weight (kilograms)",
+                        labelText: "Additional description",
+                        // labelStyle:
+                        border: OutlineInputBorder()),
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    obscureText: false,
+                    // maxLines: 3,
+                    // maxLines: 3,
+                    // suffixIcon:
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 15),),
+                ElevatedButton(onPressed:(){
+                }, child:Text("submit", style: TextStyle(color: Colors.white),)),
+                Padding(padding: EdgeInsets.only(top: 8),),
+
+              ],
+            ),
+          )
+      )
+    );
+  }
+}
