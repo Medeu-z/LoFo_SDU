425ebbb2d843afc011688ec6c8a0dd32ccf52586
Sapar Oralbek <190109012@stu.sdu.edu.kz>
2021-10-27 13:28:35 +0600

    fix bugs

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,10 @@ class MyApp extends StatelessWidget {
         '/main': (context) => const HomeWidget(),
         '/login': (context) => const SigninWidget(),
         '/signup': (context) => const SignupWidget(),
-<<<<<<< HEAD
-=======
-        '/item_information': (context) => const LoFoItemInfo()
->>>>>>> 2a8e94db0f17d3d6dd4a43e1423ce5fa0b802ae1
+// <<<<<<< HEAD
+// =======
+//         '/item_information': (context) => const LoFoItemInfo()
+// >>>>>>> 2a8e94db0f17d3d6dd4a43e1423ce5fa0b802ae1
       },
     );
   }
